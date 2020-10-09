5fe2236c0f113f14f707d75c82bceb65a5c081e6
Robson01jc <robson01jc@gmail.com>
2020-10-08 21:01:08 -0300

    Save files to hard drive, and read files from there as well.

--- main.go
+++ main.go
@@ -1,12 +1,6 @@
 package main
 
 func main() {
-	cards := deck{"Ace of Diamonds", newCard()}
-	cards = append(cards, "Six of Spades")
-
+	cards := newDeckFromFile("my_cards")
 	cards.print()
-}
-
-func newCard() string {
-	return "Five of Diamonds"
 }
